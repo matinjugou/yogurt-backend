f3f01a55b9da4d98b4d0d090c787ffdc04bbafe7
huangchao <837287450@qq.com>
2017-11-10 01:49:50 +0800

    Realize three strategies of queue arrangement: average, oldClient, tagAccuracy.

--- src/controller/api/user/queue.js
+++ src/controller/api/user/queue.js
@@ -1,5 +1,5 @@
 const Base = require('../../rest.js');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 module.exports = class extends Base {
   constructor(...args) {
     super(...args);
@@ -9,14 +9,14 @@ module.exports = class extends Base {
   async getAction() {
     const userId = this.get();
     const tags = this.get();
-    let tagList = tags.split(' ');
-    let stuff = this.model('staff').where({
+    const tagList = tags.split(' ');
+    const stuff = this.model('staff').where({
       onlineStatus: 1,
       queueCount: ['<', this.config('maxClient')]
     }).select();
     let staffArray = [];
-    for (let staff of stuff) {
-      for (let tag of tagList) {
+    for (const staff of stuff) {
+      for (const tag of tagList) {
         if (tag in staff.role.split(' ')) {
           staffArray.push(staff);
           break;
@@ -27,36 +27,29 @@ module.exports = class extends Base {
       .order('level DESC')
       .where({enable: true})
       .select();
-    for (let strategy of strategies) {
+    for (const strategy of strategies) {
       if (strategy.name === 'average') {
         staffArray.sort((a, b) => {
           return a.queueCount - b.queueCount;
         });
-        let lessCount = staffArray[0].queueCount;
-        let tmpList = [];
-        for (let staff of staffArray) {
-          if (staff.queueCount === lessCount) {
-            tmpList.push(staff);
-          }
-          else break;
+        const lessCount = staffArray[0].queueCount;
+        const tmpList = [];
+        for (const staff of staffArray) {
+          if (staff.queueCount === lessCount) tmpList.push(staff); else break;
         }
         staffArray = tmpList;
-      }
-      else if (strategy.name === 'oldClient') {
-        let pairs = this.mongo('session-pair');
-        let tmpList = [];
-        for (let staff of staffArray) {
-          if (pairs.where({userId: userId, staffId: staff.id}).select() !== []) {
-            tmpList.push(staff);
-          }
+      } else if (strategy.name === 'oldClient') {
+        const pairs = this.mongo('session-pair');
+        const tmpList = [];
+        for (const staff of staffArray) {
+          if (pairs.where({userId: userId, staffId: staff.id}).select() !== []) tmpList.push(staff);
         }
         staffArray = tmpList;
-      }
-      else if (strategy.name === 'tagAccuracy') {
-        for (let staff of staffArray) {
+      } else if (strategy.name === 'tagAccuracy') {
+        for (const staff of staffArray) {
           staff.score = 0;
-          for (let tag of staff.role.split(' ')) {
-            let tagIndex = tagList.indexOf(tag);
+          for (const tag of staff.role.split(' ')) {
+            const tagIndex = tagList.indexOf(tag);
             if (tagIndex !== -1) {
               staff.score += tagList.length - tagIndex;
             }
---
+++ src/middleware/token-verify.js
@@ -0,0 +1,9 @@
+// const jwt = require('jsonwebtoken');
+module.exports = (options, app) => {
+  return (ctx, next) => {
+    // const token = this.get();
+    // const decoded = jwt.verify(token, options.secretID);
+    // TODO:fullfill verify and ex logic of jsonwebtoken
+    return next();
+  };
+};
