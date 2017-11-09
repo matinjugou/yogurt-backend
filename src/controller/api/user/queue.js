f037d73ea911d0b814c7d1e63708b9b25ee4b8bf
huangchao <837287450@qq.com>
2017-11-10 02:15:30 +0800

    fix eslint bugs

--- src/controller/api/user/queue.js
+++ src/controller/api/user/queue.js
@@ -37,15 +37,15 @@ module.exports = class extends Base {
         for (const staff of staffArray) {
           if (staff.queueCount === lessCount) tmpList.push(staff); else break;
         }
-        staffArray = tmpList; }
-      else if (strategy.name === 'oldClient') {
+        staffArray = tmpList;
+      } else if (strategy.name === 'oldClient') {
         const pairs = this.mongo('session-pair');
         const tmpList = [];
         for (const staff of staffArray) {
           if (pairs.where({userId: userId, staffId: staff.id}).select() !== []) tmpList.push(staff);
         }
-        staffArray = tmpList; }
-      else if (strategy.name === 'tagAccuracy') {
+        staffArray = tmpList;
+      } else if (strategy.name === 'tagAccuracy') {
         for (const staff of staffArray) {
           staff.score = 0;
           for (const tag of staff.role.split(' ')) {
--- src/middleware/token-verify.js
+++ src/middleware/token-verify.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 module.exports = (options, app) => {
   return (ctx, next) => {
-    const token = this.get();
+    // const token = this.get();
     // const decoded = jwt.verify(token, options.secretID);
     // TODO:fullfill verify and ex logic of jsonwebtoken
     return next();
