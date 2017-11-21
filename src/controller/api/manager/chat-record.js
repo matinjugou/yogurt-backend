96e99cad51a73497b1ea269fe08473a1009c138d
huangchao <837287450@qq.com>
2017-11-21 22:59:53 +0800

    Fix eslint bug

--- src/controller/api/manager/chat-record.js
+++ src/controller/api/manager/chat-record.js
@@ -1,18 +1,17 @@
 const Base = require('../../base.js');
 module.exports = class extends Base {
   async getAction() {
-    const userId = this.get();
-    const staffId = this.get();
+    // const userId = this.get();
+    // const staffId = this.get();
     // const startTime = this.get();
     // const endTime = this.get();
     const index = this.get();
-    //const recordNum = this.get();
+    // const recordNum = this.get();
     if (index) {
-      const model = this.mongo('session-pair');
-      //const pair = await model.where({userId: userId,
-      //  staffId: staffId}).select()[0];
-    }
-    else {
+      // const model = this.mongo('session-pair');
+      // const pair = await model.where({userId: userId,
+      // staffId: staffId}).select()[0];
+    } else {
     }
   }
 };
