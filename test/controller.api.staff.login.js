952f1b87b647aef17d66edff1f39cb6332bf88ce
huangchao <thss15_huangc@163.com>
2018-01-02 23:41:44 +0800

    Add some test

--- test/controller.api.staff.login.js
+++ test/controller.api.staff.login.js
@@ -1,23 +1,12 @@
 const test = require('ava');
 const request = require('supertest');
 const path = require('path');
-// require(path.join(process.cwd(), 'production.js'));
-
-const Application = require('thinkjs');
-
-const instance = new Application({
-  ROOT_PATH: __dirname,
-  proxy: true, // use proxy
-  env: 'testing'
-});
-
-instance.run();
+require(path.join(process.cwd(), 'testing.js'));
 
 test('eee', t => {
-  console.error("think=", think);
+  console.error("think=", think.app);
   console.error("app=", think.app);
-
-  request(instance).post('/api/staff/login')
+  request('http://127.0.0.1:2333').post('/api/staff/login')
     .set('Content-Type', 'application/json')
     .send({
       staffId: '1_s1',
@@ -25,10 +14,8 @@ test('eee', t => {
     })
     .expect('Content-Type', /json/)
     .expect(200);
-
-  //const a = think.model('staff');
-
-  //console.error(a);
+  const a = think.model('staff');
+  console.error(a);
   t.pass();
 });
 /**
--- testing.js
+++ testing.js
@@ -3,7 +3,8 @@ const Application = require('thinkjs');
 const instance = new Application({
   ROOT_PATH: __dirname,
   proxy: true, // use proxy
-  env: 'testing'
+  env: 'testing',
+  port: 2333
 });
 
 instance.run();
