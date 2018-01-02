c22b30ea2198e5d02fe500c861da2d10e553436b
huangchao <thss15_huangc@163.com>
2018-01-02 20:36:41 +0800

    Add some test

--- test/controller.api.staff.login.js
+++ test/controller.api.staff.login.js
@@ -1,7 +1,7 @@
 const test = require('ava');
 const request = require('supertest');
 const path = require('path');
-const instance = require(path.join(process.cwd(), 'testing.js'));
+const instance = require(path.join(process.cwd(), 'development.js'));
 
 test('eee', t => {
   console.error("think=", think);
