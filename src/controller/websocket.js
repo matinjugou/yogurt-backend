7382cafa86a994862ab56ac81a59ccb57daa7f85
huangchao <thss15_huangc@163.com>
2018-01-03 17:04:26 +0800

    Enhance: Add the accuracy of message record time

--- src/controller/websocket.js
+++ src/controller/websocket.js
@@ -1,4 +1,21 @@
 const jwt = require("jsonwebtoken");
+const Format = function (date, fmt) {
+  const o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds(),
+    'q+': Math.floor((date.getMonth() + 3) / 3),
+    'S': date.getMilliseconds()
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  for (const k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+  }
+  return fmt;
+};
+
 module.exports = class extends think.Controller {
   constructor(...arg) {
     super(...arg);
@@ -83,7 +100,7 @@ module.exports = class extends think.Controller {
         size: size,
         mimeTypes: mimeType,
         msg: msg,
-        time: myDate.toLocaleDateString()
+        time: Format(myDate, "yyyy-MM-dd hh:mm:ss")
       });
     const content = {
       staffId: staffId,
@@ -96,7 +113,7 @@ module.exports = class extends think.Controller {
       size: size,
       mimeTypes: mimeType,
       msg: msg,
-      time: myDate.toLocaleDateString()
+      time: Format(myDate, "yyyy-MM-dd hh:mm:ss")
     };
     await this.mongo('sessionPair', 'mongo').insertMessage(staffId, userId, 'u_s', content);
     this.emit('sendResult',
@@ -200,7 +217,7 @@ module.exports = class extends think.Controller {
         size: size,
         mimeTypes: mimeType,
         msg: msg,
-        time: myDate.toLocaleDateString()
+        time: Format(myDate, "yyyy-MM-dd hh:mm:ss")
       });
     const content = {
       staffId: staffId,
@@ -213,7 +230,7 @@ module.exports = class extends think.Controller {
       size: size,
       mimeTypes: mimeType,
       msg: msg,
-      time: myDate.toLocaleDateString()
+      time: Format(myDate, "yyyy-MM-dd hh:mm:ss")
     };
     await this.mongo('sessionPair', 'mongo').insertMessage(staffId, userId, 's_u', content);
     this.emit('sendResult',
