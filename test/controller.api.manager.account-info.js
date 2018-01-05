d9d3d7b2b96095bff530fc4ca45d862519a384e0
effie-0 <thss15yangbf@gmail.com>
2018-01-05 20:15:55 +0800

    test: Add test for /manager/account-info

---
+++ test/controller.api.manager.account-info.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const request = require('supertest');
+const path = require('path');
+const instance = require(path.join(process.cwd(), 'testing.js'));
+
+
+describe('manager', function() {
+  before(function () {
+    this.model('manager').add({
+      companyId: 1,
+      name: 'hello',
+      tel: '123456789',
+      password: '1_m1',
+      managerId: '1_m1'
+    });
+  });
+  describe('account-info', function() {
+    it ('server should run and can get a manager', function(done){
+      const f = function() {
+        request(think.app.server).get('/api/manager/account-info')
+          .set('Content-Type', 'application/json')
+          .send({
+            managerId: '1_s1',
+            password: '1_s1'
+          })
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err;
+            console.error(res.body.data);
+            assert.equal(res.body.data.name, 'hello');
+            done();
+          });
+      };
+      setTimeout(f, 4000);
+    });
+  });
+  after(function () {
+    const model = this.model('manager');
+    model.where({managerId: '1_m1'}).delete();
+    process.exit();
+  });
+});
