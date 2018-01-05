fb441033ff9ce01622b11fb869e68704ee8ec5f2
huangchao <thss15_huangc@163.com>
2018-01-06 02:43:34 +0800

    merge test to same file

--- test/controller.api.manager.account-info.js
+++ test/controller.api.manager.account-info.js
@@ -63,3 +63,30 @@ describe('manager', function() {
   });
 });
 
+describe('staff', function() {
+  describe('login', function() {
+    it ('server should run and login should failed', function(done){
+      const f = function() {
+        request(think.app.server).post('/api/staff/login')
+          .set('Content-Type', 'application/json')
+          .send({
+            staffId: '1_s1',
+            password: '1_s2'
+          })
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err;
+            console.error(res.body.data);
+            assert.equal(res.body.data.code, 1);
+            assert.equal(res.body.data.msg, 'Staff does not exist!');
+            done();
+          });
+      };
+      setTimeout(f, 4000);
+    })
+  })
+  after(function () {
+    process.exit();
+  })
+});
